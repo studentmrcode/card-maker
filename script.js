a7699c9ef6b2ed03672e67ed0538988f69664c66
David <davidk@mrcodeswildride.com>
2018-11-16 20:55:36 +0000

    added Section text to script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Make variables for the card, title paragraph, buttons, text box, color picker, and slider.
+// Section 1: Make separate variables for the card, title paragraph, buttons, text box, color picker, and slider.
 // You can name them anything you want, but for simplicity, name them the same as the IDs.
 
 
-// Add event listeners to the buttons, text box, color picker, and slider.
+// Section 2: Add separate event listeners to the buttons, text box, color picker, and slider.
 // You can name the functions anything you want, but pick something that indicates action.
 
 
-// Make functions to change the card's color, card's title, title's color, and title's font size.
+// Section 3: Make separate functions to change the card's color, card's title, title's color, and title's font size.
