c6ebadbd238affaf974d146f8542ead25fc72c05
DanZeak <dan.zeak0898@gmail.com>
2018-11-16 00:35:08 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// Make variables for the card, title paragraph, buttons, text box, color picker, and slider.
-// You can name them anything you want, but for simplicity, name them the same as the IDs.
+// Give variables names that describe what they are.
+// Remember, event listeners have two parameters: type of event and function name.
+// Give functions names that would describe the action they are doing.
+// Keep your code organized so that it is easy to understand.
 
 
-// Add event listeners to the buttons, text box, color picker, and slider.
-// You can name the functions anything you want, but pick something that indicates action.
+// Add code below this line: --------------
+// Section 1: Make separate variables for the card, title, paragraph, buttons, text box, color picker, and slider.
 
 
-// Make functions to change the card's color, card's title, title's color, and title's font size.
+// Section 2: Add event listeners to the buttons, text box, color picker, and slider.
+
+
+// Section 3: Make separate functions to change the card's color, card's title, title's color, and title's font size.
