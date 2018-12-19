547c1c301b47ad35299ae0f7e654599f53e1e5ca
David <davidk@mrcodeswildride.com>
2018-12-19 23:57:02 +0000

    removed comments from JS

--- script.js
+++
@@ -1,9 +0,0 @@
-// Section 1: Make separate variables for the card, title paragraph, buttons, text box, color picker, and slider.
-// You can name them anything you want, but for simplicity, name them the same as the IDs.
-
-
-// Section 2: Add separate event listeners to the buttons, text box, color picker, and slider.
-// You can name the functions anything you want, but pick something that indicates action.
-
-
-// Section 3: Make separate functions to change the card's color, card's title, title's color, and title's font size.
